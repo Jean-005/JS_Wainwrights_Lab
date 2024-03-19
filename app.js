5633bc8854e7163a9b421983fa1561f000711a49
Jean <jean-ata@hotmail.co.uk>
2024-03-19 15:20:02 +0000

    added the try catch block

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
  // Function to fetch data from API
 
  async function getAllWainwrights() {
+    try {
         const response = await fetch('https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json');
         allWainwrightsData = await response.json();
         createWainwrightsList();
-    } 
+    } catch (error) {
+        console.error('There was an error fetching Wainwrights Data:', error);
+    }
+}
+
+
+
+
